8aa6435d21af841a88dae47d21243af8171925b1
jarlas Nascimento Alves <jarlasnje@gmail.com>
2018-02-10 23:50:54 -0300

    primeiro commit

---
+++ src/HelloWord.java
@@ -0,0 +1,13 @@
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		HelloWord hello = new HelloWord();
+		hello.imprimeTexto("Alô mundo");
+	}
+	
+	public void imprimeTexto(String texto) {
+		System.out.println(texto);
+	}
+
+}
